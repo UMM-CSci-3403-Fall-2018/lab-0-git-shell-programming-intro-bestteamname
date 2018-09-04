a6fa36d9815bb9facb5c9fcc4a2daf75f5f9a4f7
Avery Koranda <koran023@morris.umn.edu>
2018-09-04 10:56:51 -0500

    use rm `grep ...` to remove files with bad text

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -4,3 +4,17 @@ TARCHIVE=$1
 SCRATCH_DIR=$2
 
 tar -xzf $TARCHIVE -C $SCRATCH_DIR
+
+INNER_DIR=${TARCHIVE%.*}
+
+#delete_files=(`grep --files-with-matches 'DELETE ME!' $SCRATCH_DIR/$INNER_DIR/*`)
+
+rm `grep --files-with-matches 'DELETE ME!' $SCRATCH_DIR/$INNER_DIR/*`
+
+#for file in $SCRATCH_DIR/$INNER_DIR/*; do
+#	match_string=`grep --files-with-matches 'DELETE ME!' $file`
+#	if [ ! -z $match_string ]
+#	then
+#		rm $file
+#	fi
+#done
